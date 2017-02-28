263eb0c48f4e2f5846cd92f039d543424b4542c0
wojasek12 <wojasek12@onet.pl>
2017-02-28 13:33:05 +0100

    main modified and Test

--- app/src/main/java/com/example/wojtaszek/push/MainActivity.java
+++ app/src/main/java/com/example/wojtaszek/push/MainActivity.java
@@ -65,5 +65,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void UploadTest() {
         int i = 1 + 1;
+        int ii = 2+2;
+        int iii = i+ii;
     }
 }
---
+++ app/src/main/java/com/example/wojtaszek/push/Test.java
@@ -0,0 +1,8 @@
+package com.example.wojtaszek.push;
+
+/**
+ * Created by Wojtaszek on 28.02.2017.
+ */
+
+public class Test {
+}
